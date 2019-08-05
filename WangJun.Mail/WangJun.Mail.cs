d45d1594ee0c507dc387ea03f10da0d37d08d020
俊 汪 <turingai@outlook.com>
2019-08-06 01:42:08 +0800

    添加邮件基础代码

---
+++ WangJun.Mail/WangJun.Mail.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WangJun.Yun
+{
+    public class WangJunMail
+    {
+        protected SmtpClient smtp;
+
+        public static WangJunMail GetInst(string smtphost,int port,bool ssl,string loginId,string password)
+        {
+            var inst = new WangJunMail();
+            inst.smtp = new SmtpClient(smtphost, port);
+            inst.smtp.EnableSsl = ssl;
+            inst.smtp.Credentials = new NetworkCredential(loginId, password);
+            return inst;
+
+        }
+
+        public RES Send(string sourceAddress,string targetAddress, string title,string content)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(sourceAddress);
+                mail.To.Add(new MailAddress(targetAddress));
+                mail.Subject = title;
+                mail.Body = content;
+                mail.IsBodyHtml = true;
+                this.smtp.Send(mail);
+                return RES.OK();
+            }
+            catch (Exception ex) {
+                return RES.FAIL(ex.Message, ex.Message);
+            }
+        }
+         
+    }
+}
---
+++ WangJun.MailProcessor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WangJun.Yun;
+
+namespace WangJun.MailProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WangJunMail.GetInst("smtp.163.com", 25, false, "tushu2019", "7573Wj7573").Send("tushu2019@163.com", "tushu2019@163.com", "测试", "测试111");
+            Console.ReadKey();
+        }
+    }
+}
